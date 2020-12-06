36d1e4bad0732030e44d9b2a24e7f398bcba4765
LuoShuqi <songcom@live.com>
2020-12-06 20:17:12 +0800

    drawer_setting

--- lib/page/drawer_setting/index.dart
+++ lib/page/drawer_setting/index.dart
@@ -17,14 +17,37 @@ class _SettingPage extends State<SettingPage> {
     @override
   Widget build(BuildContext context) {
         //获取路由参数  
-    // var speakerId=ModalRoute.of(context).settings.arguments;
-    List<Widget> list = <Widget> [
+    var pageTTitle=ModalRoute.of(context).settings.arguments;
+    List<Widget> list = [];
+    List<Widget> listSetting = <Widget> [
       CustomListItem(title: '电子邮件订阅', urlName:'/',),
       CustomListItem(title: '改进计划', urlName:'/',),
     ];
+    List<Widget> listHelp = <Widget> [
+      CustomListItem(title: '快速指南', urlName:'/',),
+      CustomListItem(title: '用户手册', urlName:'/',),
+      CustomListItem(title: '联系我们', urlName:'/',),
+    ];
+    List<Widget> listStore = <Widget> [
+      CustomListItem(title: '电子邮件订阅', urlName:'/',),
+      CustomListItem(title: '改进计划', urlName:'/',),
+    ];
+    List<Widget> listAbout = <Widget> [
+      CustomListItem(title: '使用条款', urlName:'/',),
+      CustomListItem(title: '免费和开源软件', urlName:'/',),
+    ];
+    switch(pageTTitle) {
+      case '应用设置': list = listSetting; break;
+      case '帮助': list = listHelp; break;
+      case '商店': list = listStore; break;
+      case '关于音箱': list = listAbout; break;
+      // Defaul list = listSetting;
+    }
+
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('应用设置'),
+        title: Text(pageTTitle),
       ),
        body: Padding(
         padding: EdgeInsets.all(18),
--- lib/page/home/drawer_list.dart
+++ lib/page/home/drawer_list.dart
@@ -3,7 +3,7 @@ import 'package:bluetooth_speaker/models/source.dart';
 class DrawerListView extends StatelessWidget {
   DrawerListView({Key key}) : super(key: key);
   List<DrawerModle> list = [
-    DrawerModle.fromJson({'title': '设置', 'urlName': '/setting', 'keyName': 'setting', 'icon': Icon(Icons.settings)}),
+    DrawerModle.fromJson({'title': '应用设置', 'urlName': '/setting', 'keyName': 'setting', 'icon': Icon(Icons.settings)}),
     DrawerModle.fromJson({'title': '帮助', 'urlName': '/setting', 'keyName': 'setting_help', 'icon': Icon(Icons.help_outline)}),
     DrawerModle.fromJson({'title': '商店', 'urlName': '/setting', 'keyName': 'setting_store', 'icon': Icon(Icons.shopping_cart)}),
     DrawerModle.fromJson({'title': '关于音箱', 'urlName': '/setting', 'keyName': 'setting_about', 'icon': Icon(Icons.info_outline)})
